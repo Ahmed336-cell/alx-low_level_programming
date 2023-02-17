9f38676e8bfec2a80d302fc190b497a121d48b93
Ahmed Maher <65620946+Ahmed336-cell@users.noreply.github.com>
2023-02-17 17:54:40 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * main - main function to run
- * Description: Get a random number and check its last digit, compare it with 5
+ * Description:  prints the alphabet in lowercase, followed by a new line.
  * Return: 0
  */
 int main(void)
